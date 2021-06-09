71fa6060733e105d591aef0409b6c66b0c9b9f9a
Warix <39554785+warix8@users.noreply.github.com>
2021-06-09 18:40:42 +0200

    🎉 New useful Logger

--- src/utils/Logger.js
+++ src/utils/Logger.js
@@ -5,21 +5,31 @@ const chalk = require("chalk");
 class Logger {
     constructor(title){
         this.loggerTitle = title;
+        this._types = {
+            log: "white",
+            info: "blue",
+            sucess: "green",
+            debug: "magenta",
+            warn: "yellow",
+            error: "red"
+        },
+        this._originalConsole = Object.assign({}, console);
+        this._init();
     }
 
-    info(content) {
-        return console.log(this.getDate() + chalk.bgBlack.blue(`[${this.loggerTitle}] ${content}`));
+    _init() {
+        for(const type in this._types){
+            console.log(type)
+            this[type] = (content) => {
+                this._originalConsole.log(this._getDate() + chalk.bgBlack[this._types[type]](`[${this.loggerTitle}] ${content}`));
+            }
+            console[type] = (content) => {
+                this[type](content);
+            }
+        }
     }
 
-    sucess(content){
-        return console.log(this.getDate() + chalk.bgBlack.green(`[${this.loggerTitle}] ${content}`));
-    }
-
-    error(content) {
-        return console.error(this.getDate() + chalk.bgBlack.red(`[${this.loggerTitle}] ${content}`));
-    }
-
-    getDate() {
+    _getDate() {
         return chalk.gray(`[${new Date(Date.now()).toLocaleString("FR-fr", { timeZone: "Europe/Paris" })}]`)
     }
 }
