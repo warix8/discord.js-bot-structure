9a8daf4af3633d34838f2228a9d2d2ca6cc49c05
Warix <39554785+warix8@users.noreply.github.com>
2026-04-27 20:24:57 +0200

    Should probably fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 export default defineConfig({
 	datasource: {
-		url: env("DATABASE_URL")
+		url: process.env.DATABASE_URL ?? "file:./dev.db"
 	}
 });
